96a06877a11481ed1fb027a61e4e19a2a35f470c
Angelo Esam <angloesam61@gmail.com>
2024-10-21 00:46:16 +0300

    after FadIn animation

--- src/components/LightBox.jsx
+++ src/components/LightBox.jsx
@@ -1,35 +1,24 @@
+
 import React from "react";
 
 const LightBox = ({ image, onClose, onNext, onPrev }) => {
   return (
     <div className="lightBox" onClick={onClose}>
       <div className="lightBox-item" onClick={(e) => e.stopPropagation()}>
-        <div
-          className="image-container"
-          style={{
-            backgroundImage: `url(${image.src})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center center",
-            position: "relative",
-          }}
-        >
-          <i
-            id="close"
-            className="fa-regular fa-circle-xmark"
-            onClick={onClose}
-          ></i>
+        <div className="image-container">
+          <i id="close" className="fa-regular fa-circle-xmark" onClick={onClose}></i>
+          <i id="prev" className="fa-regular fa-circle-left" onClick={onPrev}></i>
+          <i id="next" className="fa-regular fa-circle-right" onClick={onNext}></i>
 
-          <i
-            id="prev"
-            className="fa-regular fa-circle-left"
-            onClick={onPrev}
-          ></i>
-
-          <i
-            id="next"
-            className="fa-regular fa-circle-right"
-            onClick={onNext}
-          ></i>
+          <div
+            className="image"
+            style={{
+              backgroundImage: `url(${image.src})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center center",
+              height: "500px",
+            }}
+          ></div>
         </div>
       </div>
     </div>
